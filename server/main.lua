ff2feb91172e7dc0f1fd63f5c7879bf9db28e782
MaDHouSe <madhouse1979@gmail.com>
2023-12-19 12:58:34 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,6 @@
---[[ ===================================================== ]]--
---[[             MH Airdrops Script by MaDHouSe            ]]--
---[[ ===================================================== ]]--
-
+--[[ ===================================================== ]] --
+--[[             MH Airdrops Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
 local QBCore = exports['qb-core']:GetCoreObject()
 local cooldown = math.random(Config.Cooldown.min, Config.Cooldown.max)
 
@@ -10,7 +9,7 @@ local function SpawnAirdrop()
         local randomLocation = math.random(1, #Config.Locations)
         for k, id in pairs(QBCore.Functions.GetPlayers()) do
             local target = QBCore.Functions.GetPlayer(id)
-            if not Config.NotAllowedJobs[target.PlayerData.job.name] then 
+            if not Config.NotAllowedJobs[target.PlayerData.job.name] then
                 TriggerClientEvent('mh-airdrops:client:deleteObj', id)
                 TriggerClientEvent('mh-airdrops:client:airdrop', id, randomLocation)
                 TriggerClientEvent('mh-airdrops:client:notify', id, Lang:t('notify.airdrop_landed'), "success", 10000)
@@ -24,13 +23,13 @@ end
 local function StartAirdropLoop()
     while true do
         while cooldown > 0 do
-            --print(string.format('%s Minuten tot de volgende Airdrop Spawn', cooldown))
+            -- print(string.format('%s Minuten tot de volgende Airdrop Spawn', cooldown))
             Citizen.Wait(60 * 1000)
             cooldown = cooldown - 1
         end
         cooldown = math.random(Config.Cooldown.min, Config.Cooldown.max)
         if #QBCore.Functions.GetPlayers() >= Config.MinPlayerOnline then
-            SpawnAirdrop() 
+            SpawnAirdrop()
         end
     end
 end
@@ -51,11 +50,11 @@ QBCore.Functions.CreateUseableItem(Config.Item1, function(source, item)
 end)
 
 QBCore.Functions.CreateUseableItem(Config.Item2, function(source, item)
-	local src = source
+    local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.RemoveItem(Config.Item2, 1, false)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Item2], 'remove', 1)
-	local randomLocation = math.random(1, #Config.Locations)
+    local randomLocation = math.random(1, #Config.Locations)
     TriggerClientEvent('mh-airdrops:client:deleteObj', src)
     TriggerClientEvent('mh-airdrops:client:airdrop', src, randomLocation)
     TriggerClientEvent('mh-airdrops:client:notify', src, Lang:t('notify.airdrop_landed'), "success", 5000)
@@ -73,9 +72,11 @@ RegisterNetEvent('mh-airdrops:server:getloot', function(ObjNetId)
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerClientEvent('mh-airdrops:client:deleteObj', -1, ObjNetId)
     TriggerClientEvent('mh-airdrops:client:notify', -1, Lang:t('notify.airprop_caught'), "success", 5000)
-    
-    if Config.UseDailyActivities then TriggerClientEvent('mh-dailyactivities:client:updateTask', src, Config.DailyTaskID) end    
-    
+
+    if Config.UseDailyActivities then
+        TriggerClientEvent('mh-dailyactivities:client:updateTask', src, Config.DailyTaskID)
+    end
+
     local whatBooty = Config.whatBooty
     if Config.UseItemsDrop then
         whatBooty = 1
@@ -102,12 +103,12 @@ RegisterNetEvent('mh-airdrops:server:getloot', function(ObjNetId)
             TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add', random)
         end
 
-    elseif whatBooty == 3 then 
+    elseif whatBooty == 3 then
         local ammo = Config.Items['ammo'][math.random(1, #Config.Items['ammo'])]
         Player.Functions.AddItem(ammo.name, ammo.amount, false)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.amount], 'add', ammo.amount)
 
-    elseif whatBooty == 4 then  
+    elseif whatBooty == 4 then
         local weapon = Config.Items['weapons'][math.random(1, #Config.Items['weapons'])]
         Player.Functions.AddItem(weapon.model, 1, false, nil)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[weapon.model], 'add', 1)
