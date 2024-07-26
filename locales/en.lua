147e7155c6f1300f5196905aa18e3236b8b850d4
MaDHouSe <madhouse1979@gmail.com>
2024-07-26 11:52:23 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -6,6 +6,8 @@ local Translations = {
         text = "Airdrop",
         drawtext = "[LEFT ALT] + [R-MOUSE]",
         drawNoAccess = "~r~Access denied~w~",
+        locked = "~r~Locked~w~",
+        unlocked = "~g~Unlocked~w~",
     },
     notify = {
         airdrop_landed = "Airdrop has landed, get there quickly before someone else catches it!",
