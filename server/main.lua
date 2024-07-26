be8b8b21c6c242ae2308828f33b13b75ec25bcce
MaDHouSe <madhouse1979@gmail.com>
2024-07-26 11:52:57 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -30,7 +30,6 @@ local function SpawnAirdrop()
                 TriggerClientEvent('mh-airdrops:client:deleteObj', id)
                 TriggerClientEvent('mh-airdrops:client:airdrop', id, randomLocation)
                 TriggerClientEvent('mh-airdrops:client:notify', id, Lang:t('notify.airdrop_landed'), "success", 10000)
-                UnlockCrate()
             end
         end
     else
@@ -56,22 +55,11 @@ QBCore.Commands.Add("airdrop", "Drop an airdrop", {}, true, function(source)
     SpawnAirdrop()
 end, 'admin')
 
-QBCore.Functions.CreateUseableItem(Config.Item1, function(source, item)
+QBCore.Functions.CreateUseableItem(Config.Item, function(source, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveItem(Config.Item1, 1, false)
-    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Item1], 'remove', 1)
-    local randomLocation = math.random(1, #Config.Locations)
-    TriggerClientEvent('mh-airdrops:client:deleteObj', src)
-    TriggerClientEvent('mh-airdrops:client:airdrop', src, randomLocation)
-    TriggerClientEvent('mh-airdrops:client:notify', src, Lang:t('notify.airdrop_landed'), "success", 5000)
-end)
-
-QBCore.Functions.CreateUseableItem(Config.Item2, function(source, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveItem(Config.Item2, 1, false)
-    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Item2], 'remove', 1)
+    Player.Functions.RemoveItem(Config.Item, 1, false)
+    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Item], 'remove', 1)
     local randomLocation = math.random(1, #Config.Locations)
     TriggerClientEvent('mh-airdrops:client:deleteObj', src)
     TriggerClientEvent('mh-airdrops:client:airdrop', src, randomLocation)
@@ -85,48 +73,75 @@ AddEventHandler('onResourceStart', function(resourceName)
     end
 end)
 
+RegisterNetEvent('mh-airdrops:server:unlock', function()
+    UnlockCrate()
+end)
+
 RegisterNetEvent('mh-airdrops:server:getloot', function(ObjNetId)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerClientEvent('mh-airdrops:client:deleteObj', -1, ObjNetId)
-    NotifyPlayers(src)    
+    NotifyPlayers(src)
 
-    local whatBooty = Config.whatBooty
-    if Config.UseItemsDrop then
-        whatBooty = 1
-    elseif Config.UseRareItemsDrop then
-        whatBooty = math.random(1, 2)
-    elseif Config.UseAmmoDrop then
-        whatBooty = math.random(1, 2)
-    elseif Config.UseWeaponDrop then
-        whatBooty = math.random(1, 3)
-    elseif Config.UseAllDrops then
-        whatBooty = math.random(1, 4)
-    end
+    if Config.UseItems then
+        local whatBooty = Config.whatBooty
+        if Config.UseItemsDrop then whatBooty = 1 end
+        if Config.UseRareItemsDrop then whatBooty = math.random(1, 2) end
+        if Config.UseAmmoDrop then whatBooty = math.random(1, 2) end
+        if Config.UseWeaponDrop then whatBooty = math.random(1, 3) end
+        if Config.UseAllDrops then whatBooty = math.random(1, 4) end
 
-    if whatBooty == 1 then
-        local item = Config.Items.basic[math.random(1, #Config.Items.basic)]
-        local random = math.random(10, 15)
-        Player.Functions.AddItem(item.name, random, false)
-        TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add', random)
-
-    elseif whatBooty == 2 then
-        local random = math.random(1, #Config.Items.rare)
-        local item = Config.Items.rare[random]
-        for _ = 1, math.random(5, 10), 1 do
-            Player.Functions.AddItem(item.name, random, false)
-            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add', random)
+        if whatBooty == 1 then
+            local item = Config.Items.basic[math.random(1, #Config.Items.basic)]
+            if QBCore.Shared.Items[item.name] then
+                local random = math.random(10, 15)
+                Player.Functions.AddItem(item.name, random, false)
+                TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add', random)
+            else
+                print(item.name..' does not exsist in qb-core/shared/items.lua file...')
+            end
         end
 
-    elseif whatBooty == 3 then
-        local ammo = Config.Items['ammo'][math.random(1, #Config.Items['ammo'])]
-        Player.Functions.AddItem(ammo.name, ammo.amount, false)
-        TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.amount], 'add', ammo.amount)
+        if whatBooty == 2 then
+            local random = math.random(1, #Config.Items.rare)
+            local item = Config.Items.rare[random]
+            if QBCore.Shared.Items[item.name] then
+                for _ = 1, math.random(5, 10), 1 do
+                    Player.Functions.AddItem(item.name, random, false)
+                    TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add', random)
+                end
+            else
+                print(item.name..' does not exsist in qb-core/shared/items.lua file...')
+            end
+        end
 
-    elseif whatBooty == 4 then
-        local weapon = Config.Items['weapons'][math.random(1, #Config.Items['weapons'])]
-        Player.Functions.AddItem(weapon.model, 1, false, nil)
-        TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[weapon.model], 'add', 1)
+        if whatBooty == 3 then
+            local ammo = Config.Items['ammo'][math.random(1, #Config.Items['ammo'])]
+            Player.Functions.AddItem(ammo.name, ammo.amount, false)
+            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.amount], 'add', ammo.amount)
+        end
+
+        if whatBooty == 4 then
+            local weapon = Config.Items['weapons'][math.random(1, #Config.Items['weapons'])]
+            Player.Functions.AddItem(weapon.model, 1, false, nil)
+            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[weapon.model], 'add', 1)
+        end
+    end
+
+    if Config.UseMarkedbills then
+        local chance = math.random(1, 100)
+        if chance < 25 then
+            local amount = math.random(1, 3)
+            local info = { worth = math.random(Config.Markedbills.min, Config.Markedbills.max) }
+            Player.Functions.AddItem('markedbills', amount, false, info)
+            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items['markedbills'], 'add', amount)
+        end
+    else
+        local chance = math.random(1, 100)
+        if chance < 25 then
+            local amount = math.random(Config.Cash.min, Config.Cash.max)
+            Player.Functions.AddMoney(Config.CashItem, amount, nil)
+        end
     end
 
     if Config.UseJackpotItem then
@@ -136,4 +151,5 @@ RegisterNetEvent('mh-airdrops:server:getloot', function(ObjNetId)
             TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[Config.JackpotItem], 'add', Config.JackpotAmount)
         end
     end
+
 end)
